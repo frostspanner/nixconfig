18faedc15ac9fbd19954ef88970503a118c47fb2
frostspanner <frostspanner@github.com>
2025-10-18 14:06:48 -0400

    feat(de): add niri

---
+++ desktops/niri/default.nix
@@ -0,0 +1,47 @@
+{pkgs, ...}:
+{
+programs.niri.enable = true;
+programs.waybar.enable = true;
+services.hypridle.enable = true;
+services.flatpak.enable = true;
+programs.hyprlock.enable = true;
+imports = [
+../../packages/desktop.nix
+../../packages/cli.nix
+../../packages/twm.nix
+../../packages/dev.nix
+];
+environment.systemPackages = with pkgs; [
+alacritty
+fuzzel
+xwayland-satellite
+nautilus #needed for file picker
+];
+  services.udev.packages = [pkgs.swayosd];
+
+  system.activationScripts = {
+    # swayosd cannot set brightness issue on NixOS see
+    # https://github.com/ErikReider/SwayOSD/issues/12#issuecomment-1950581102
+    fix-brightness-file-permission.text = ''       
+      chgrp video /sys/class/backlight/nvidia_0/brightness 
+      chmod g+w /sys/class/backlight/nvidia_0/brightness 
+      '';
+  };
+
+  systemd.services.swayosd-libinput-backend = {
+    description = "SwayOSD LibInput input listening backend.";
+    documentation = ["https://github.com/ErikReider/SwayOSD"];
+    wantedBy = [
+      "graphical.target"
+    ];
+    partOf = ["graphical.target"];
+    after = ["graphical.target"];
+
+    serviceConfig = {
+      Type = "dbus";
+      BusName = "org.erikreider.swayosd";
+      ExecStart = "${pkgs.swayosd}/bin/swayosd-libinput-backend";
+      Restart = "on-failure";
+    };
+  };
+}
